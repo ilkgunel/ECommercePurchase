92e689a2890de70c466cfd844cd0f31692b38210
ilkgunel <ilkgunel93@gmail.com>
2025-08-11 01:15:02 +0300

    - Adding JavaDoc for BillController

--- src/main/java/com/ecommercepurchase/controller/BillController.java
+++ src/main/java/com/ecommercepurchase/controller/BillController.java
@@ -22,6 +22,12 @@ public class BillController {
         this.billInterface = billInterface;
     }
 
+    /**
+     * @author ilkgunel93@gmail.com
+     * This is a REST API endpoint to take a BillRequest from client and to insert a Bill to database
+     * @param billRequest contains the bill information which is being wanted to save.
+     * @return a parameter which is in ResponseEntity<BillResponse> type to inform client bill saved or rejected.
+     */
     @PostMapping("/create")
     public ResponseEntity<BillResponse> createBill(@RequestBody BillRequest billRequest) {
         BillResponse billResponse = billInterface.addBill(billRequest);
@@ -29,12 +35,26 @@ public class BillController {
         return new ResponseEntity<BillResponse>(billResponse, HttpStatus.CREATED);
     }
 
+    /**
+     * @author ilkgunel93@gmail.com
+     * This is a REST API endpoint to return approved bills of sales person who is autheticated.
+     * @param userDetails is not coming from client, Spring Boot fill is for us to detect who authenticated user is. We're getting userName property of authenticated user.
+     * @return a parameter which is in ResponseEntity<List<Bill>>. The client will get a list to get approved bills.
+     * @apiNote this service and below service can be combined, the bill status can be obtained from customer as path param or path variable. Actually, no need write 2 services.
+     */
     @GetMapping("/list/approved")
     public ResponseEntity<List<Bill>> getApprovedBills(@AuthenticationPrincipal UserDetails userDetails) {
         List<Bill> approvedBills = billInterface.getBillsByStatus(userDetails.getUsername(), true);
         return ResponseEntity.ok(approvedBills);
     }
 
+    /**
+     * @author ilkgunel93@gmail.com
+     * This is a REST API endpoint to return rejected bills of sales person who is autheticated.
+     * @param userDetails is not coming from client, Spring Boot fill is for us to detect who authenticated user is. We're getting userName property of authenticated user.
+     * @return a parameter which is in ResponseEntity<List<Bill>>. The client will get a list to get rejected bills.
+     * @apiNote this service and above service can be combined, the bill status can be obtained from customer as path param or path variable. Actually, no need write 2 services.
+     */
     @GetMapping("/list/rejected")
     public ResponseEntity<List<Bill>> getRejectedBills(@AuthenticationPrincipal UserDetails userDetails) {
         List<Bill> rejectedBillList = billInterface.getBillsByStatus(userDetails.getUsername(), false);
